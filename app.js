4f63c9bee801f2b80d1e6f170d7994fd9b452733
SHAON1028 <shaonkhairul@gmail.com>
2023-03-18 15:46:06 +0600

    model

--- app.js
+++ app.js
@@ -12,11 +12,19 @@ const productSchema  = mongoose.Schema({
     type:String,
     required:[true,"Please a Name for this product"],
     trim:true,
-    unique:[,"Name must be unique"],
+    unique:[true,"Name must be unique"],
     minLength:[3,"Name must be three characters"],
     maxLength:[100,"Name is too long"]
 
   },
+  // name: {
+  //   type: String,
+  //   required: [true, "Please provide a name for this product."],
+  //   trim: true,
+  //   unique: [true, "Name must be unique"],
+  //   minLength: [3, "Name must be at least 3 characters."],
+  //   maxLenght: [100, "Name is too large"],
+  // },
   description:{
     type:String,
     required:true
@@ -56,7 +64,7 @@ const productSchema  = mongoose.Schema({
     required:true,
     enum:{
      values: ["in-stock","out-of-stock","discontinued"],
-      message:"Status can't be [VALUE]"
+     message:"Status can't be [VALUE]"
     }
   },
   // createdAt:{
@@ -67,11 +75,11 @@ const productSchema  = mongoose.Schema({
   //   type:Date,
   //   default:Date.now
   // }
-  supplier:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"Supplier",
-    required:true
-  },
+  // supplier:{
+  //   type:mongoose.Schema.Types.ObjectId,
+  //   ref:"Supplier",
+  //   required:true
+  // },
   // embedded schema
   categories:[{
     name:{
@@ -82,13 +90,67 @@ const productSchema  = mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId
   }]
 
-},{
+},
+{
   timestamps:true
 })
+// 
+
+// middlewares for mongoose :pre, post
+productSchema.pre('save',function(next){
+
+  //this -> 
+   console.log(' Before saving data');
+     if (this.quantity == 0) {
+      this.status = 'out-of-stock'
+    }
+
+   next()
+ })
+// jekono fuction inject korte pari
+productSchema.methods.logger= function(){
+  console.log(` Data saved for ${this.name}`);
+}
+
+//  productSchema.post('save',function(doc,next){
+//   console.log('After saving data');
+
+//   next()
+// })
+// Schema --> models --> Query
+
+// model
+
+const Product = mongoose.model("Product",productSchema)
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
+
+app.post('/api/v1/product', async(req, res) => {
+  try{
+    //save
+    // const product = new Product(req.body)
+    // const result =await product.save()
+
+    // create
+    const result = await Product.create(req.body)
+    result.logger()
+  res.status(200).send({
+    status:'success',
+    message:"data saved successfully",
+    data:result
+  })
+  }
+  catch(error){
+    res.status(400).send({
+      status:'fail',
+      message:"data not saved",
+      error:error.message
+    })
+  }
+})
+
 
 
 module.exports = app;
